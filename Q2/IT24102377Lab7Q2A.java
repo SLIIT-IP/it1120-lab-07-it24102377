1d7fcfce513a9c7e4cb7e2b6ceb64929c09945e6
it24102377 <it24102377@my.sliit.lk>
2024-09-19 16:43:06 +0530

    IT24102377Lab7Q2A to Q2

---
+++ Q2/IT24102377Lab7Q2A.java
@@ -0,0 +1,10 @@
+public class IT24102377Lab7Q2A {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println("$ ");
+            }
+            System.out.println();
+        }
+    }
+}
